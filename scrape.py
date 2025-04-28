08c5df641e0c12c4c2d13c0e752688cde5d1c24f
NuQCLanh <nnguyen1265@gmail.com>
2025-04-28 14:42:28 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ import os
 
 
 url = "https://n-production-serial-code.sekai-en.com/api/serial-code"
-user_id = "413727xxxxxxxxxxxx"  # replace with your actual user ID if needed
+user_id = "224361629125529601"  # replace with your actual user ID if needed
 
 headers = {
     "Host": "n-production-serial-code.sekai-en.com",
